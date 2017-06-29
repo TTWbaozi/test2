069da18afdf0571767469ae2f59c73b32b1108b3
tanqinwen <tangqinweng@163.com>
2017-06-29 10:50:22 +0800

    213123

--- 哈哈哈哈/哈哈哈哈/ViewController.m
+++ 哈哈哈哈/哈哈哈哈/ViewController.m
@@ -20,6 +20,10 @@
     
     
     self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor redColor];
     
 }
 
