a1b871b0c261e52122e8dba649593dd6234b51d2
tanqinwen <tangqinweng@163.com>
2017-06-29 10:30:23 +0800

    223333

---
+++ 哈哈哈哈/哈哈哈哈/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  哈哈哈哈
+//
+//  Created by 谭覃文 on 2017/6/29.
+//  Copyright © 2017年 青橙软件. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 哈哈哈哈/哈哈哈哈/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  哈哈哈哈
+//
+//  Created by 谭覃文 on 2017/6/29.
+//  Copyright © 2017年 青橙软件. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
